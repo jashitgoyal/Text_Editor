d22d4f60d1ba44a5b3906163d7781a9ae5df5e02
Jashit Goyal <jashitgoyal@Jashits-Laptop.local>
2024-08-22 11:16:01 +0530

    Reading Keypress From User and terminating program on Q press

--- jashit.c
+++ jashit.c
@@ -1,3 +1,10 @@
-int main(){
- return 0;
+#include <unistd.h>
+#include<stdio.h>
+int main() {
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+    printf("%c",c);
+  };
+  
+  return 0;
 }
