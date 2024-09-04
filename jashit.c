834baf9f5c46c71a3d2b95ccbdf185864e63a543
Jashit Goyal <jashitgoyal@Jashits-Laptop.local>
2024-09-04 17:20:46 +0530

    Added more scrolling adjustment

--- jashit.c
+++ jashit.c
@@ -241,7 +241,7 @@ void editorRefreshScreen() {
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,(E.cx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
   
 
@@ -304,16 +304,24 @@ int getWindowSize(int *rows, int *cols) {
 
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
         E.cx--;
+      } else if (E.cy > 0) {
+        E.cy--;
+        E.cx = E.row[E.cy].size;
       }
       break;
     case ARROW_RIGHT:
-     
+      if (row && E.cx < row->size) {
         E.cx++;
-      
+      } else if (row && E.cx == row->size) {
+        E.cy++;
+        E.cx = 0;
+      }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
@@ -325,6 +333,12 @@ void editorMoveCursor(int key) {
         E.cy++;
       }
       break;
+  }
+
+   row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen;
   }
 }
 
