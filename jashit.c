a8ebea4099469effccb6a03ed4e731b71ce5bb90
Jashit Goyal <jashitgoyal@Jashits-Laptop.local>
2024-09-04 12:29:40 +0530

    Reading Already created files into the current terminal size

--- jashit.c
+++ jashit.c
@@ -1,4 +1,8 @@
 // Headers
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -37,13 +41,25 @@ struct editorConfig {
   int screenrows;
   int screencols;
   int numrows;
-  erow row;
+  erow *row;
 
   struct termios orig_termios;
 };
 
 struct editorConfig E;
 
+
+// Row Operations
+
+void editorAppendRow(char *s, size_t len) {
+   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  int at = E.numrows;
+  E.row[at].size = len;
+  E.row[at].chars = malloc(len + 1);
+  memcpy(E.row[at].chars, s, len);
+  E.row[at].chars[len] = '\0';
+  E.numrows++;
+}
 
 
 
@@ -56,17 +72,7 @@ void die(const char *s) {
   exit(1);
 }
 
-// FILE I/O
 
-void editorOpen() {
-  char *line = "Hello, world!";
-  ssize_t linelen = 13;
-  E.row.size = linelen;
-  E.row.chars = malloc(linelen + 1);
-  memcpy(E.row.chars, line, linelen);
-  E.row.chars[linelen] = '\0';
-  E.numrows = 1;
-}
 // Terminal Functions
 
 
@@ -172,7 +178,7 @@ void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
     if (y >= E.numrows) {
-      if (y == E.screenrows / 3) {
+      if (E.numrows == 0 && y == E.screenrows / 3) {
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome),
           "Jashit editor -- version %s", JASHIT_VERSION);
@@ -188,9 +194,9 @@ void editorDrawRows(struct abuf *ab) {
         abAppend(ab, "~", 1);
       }
     } else {
-      int len = E.row.size;
+      int len = E.row[y].size;
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab, E.row.chars, len);
+      abAppend(ab, E.row[y].chars, len);
     }
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
@@ -198,6 +204,8 @@ void editorDrawRows(struct abuf *ab) {
     }
   }
 }
+
+
 
 
 // Cleaning Stuff
@@ -219,6 +227,24 @@ void editorRefreshScreen() {
   write(STDOUT_FILENO, ab.b, ab.len);
   
   abFree(&ab);
+}
+
+// FILE I/O
+
+void editorOpen(char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp) die("fopen");
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  while ((linelen = getline(&line, &linecap, fp)) != -1) {
+    while (linelen > 0 && (line[linelen - 1] == '\n' ||
+                           line[linelen - 1] == '\r'))
+      linelen--;
+    editorAppendRow(line, linelen);
+  }
+  free(line);
+  fclose(fp);
 }
 
 /*** input ***/
@@ -319,17 +345,16 @@ void initEditor() {
   E.cx = 0;
   E.cy = 0;
   E.numrows = 0;
-
+  E.row = NULL;
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main(){
-
- enableRawMode();
- initEditor();
- editorOpen();
-
-
+int main(int argc, char *argv[]) {
+  enableRawMode();
+  initEditor();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
   while (1) {
     editorRefreshScreen();
     editorProcessKeypress();
